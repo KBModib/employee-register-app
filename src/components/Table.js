4c5c477b52fa85b84158fac79267ffff0476f737
KBModib <99152532+KBModib@users.noreply.github.com>
2022-06-17 23:12:41 +0200

    Update Table.js

--- src/components/Table.js
+++ src/components/Table.js
@@ -6,8 +6,8 @@ export default function Table() {
     const lname = form.getElementByClassName('lastName').item(0).textContent
     const email = form.getElementByClassName('email').item(0).textContent
     const employeeid = form.getElementByClassName('employeeID').item(0).textContent
-    const imageSRC = form.getElementByClassName('image').item(0).textContent
-    const time = form.getElementByClassName('time').item(0).text
+    const imageSRC = form.getElementByClassName('image').item(0).src
+    const time = form.getElementByClassName('time').item(0).textContent
 
     logDataEntry(fname, lname, email, employeeid, imageSRC, time); {
       const dataRow = document.createElement('div');
@@ -16,8 +16,17 @@ export default function Table() {
         
     }
   return (
-    <div>
-      
+      <>
+    <div className="data-row">
+        <span className="data-fname table-header table-column">First Name</span>
+        <span className="data-lname table-header table-column">Last Name</span>
+        <span className="data-email table-header table-column">Email</span>
+        <span className="data-employeeid table-header table-column">Employee ID</span>
+        <span className="data-time table-header table-column">Timestamp</span>
+        <span className="data-image table-header table-column">Image</span>
     </div>
+    <div className="data-items>
+      </div>
+    <div   
   )
 }
